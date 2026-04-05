fd6a21c0f2c0f7a004f9833e508641250dd68663
Aryaman Modi <69678251+aryamanm535@users.noreply.github.com>
2026-04-05 17:57:05 -0500

    Add: design-twitter-feed - submission-10

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-10.java
@@ -0,0 +1,61 @@
+class Twitter {
+    private Map<Integer, List<int[]>> data;
+    private Map<Integer, Set<Integer>> following;
+    private int time;
+
+    public Twitter() {
+        data = new HashMap<>();
+        following = new HashMap<>();
+        time = 0;
+    }
+    
+    public void postTweet(int userId, int tweetId) {
+        time++;
+        data.putIfAbsent(userId, new ArrayList<>());
+        data.get(userId).add(new int[]{tweetId, time});
+    }
+    
+    public List<Integer> getNewsFeed(int userId) {
+        List<Integer> res = new ArrayList<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b)->a[1] - b[1]);
+        if (data.containsKey(userId)) {
+            for (int[] tweet : data.get(userId)) {
+                pq.offer(tweet);
+                if (pq.size() > 10) {
+                    pq.poll();
+                }
+            }
+        }
+        
+        if(following.containsKey(userId)){
+            for(int followeeId : following.get(userId)){
+                if (!data.containsKey(followeeId)) 
+                    continue;
+                for(int[] tweet : data.get(followeeId)){
+                    pq.offer(tweet);
+                    if(pq.size() > 10)
+                        pq.poll();
+                }
+            }
+        }
+
+        while(!pq.isEmpty())
+            res.add(0, pq.poll()[0]);
+
+        return res;
+        
+    }
+    
+    public void follow(int followerId, int followeeId) {
+        if (followerId == followeeId) 
+            return;
+        following.putIfAbsent(followerId, new HashSet<>());
+        following.get(followerId).add(followeeId);
+    }
+    
+    public void unfollow(int followerId, int followeeId) {
+        if(!following.containsKey(followerId))
+            return;
+        following.get(followerId).remove(followeeId);
+    }
+}
